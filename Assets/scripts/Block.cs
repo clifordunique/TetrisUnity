1f6ba87cffeba88d7610dd3f5eb0e0129fb28b54
matkomatko <matko.matko@hotmail.com>
2020-10-21 21:01:50 +0200

    finished lines (probably)

--- Assets/scripts/Block.cs
+++ Assets/scripts/Block.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.scripts
 {
     class Block
     {
         public int Number { get; set; }
-
+        //public GameObject gameObject{ get; set; }
+        
         public Block(int Number_ = 0)
         {
             Number = Number_;
+            //gameObject = null;
         }
     }
 }
--- Assets/scripts/MainBehaviour.cs
+++ Assets/scripts/MainBehaviour.cs
@@ -10,7 +10,7 @@ public class MainBehaviour : MonoBehaviour
     public bool goDown = true;
     public int spawnPiece = -1;
     GameObject[] TetrisPiece = new GameObject[4];
-    Block[,] field = new Block[22, 10];
+    int[,] field = new int[10, 22];
     public float fallingSpeed = 0.2f;
     int[] x = new int[4];
     int[] y = new int[4];
@@ -23,11 +23,11 @@ public class MainBehaviour : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             string a = "";
-            for (int i = 0; i < 22; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < 22; j++)
                 {
-                    a += field[i, j].Number;
+                    a += field[i, j];
                 }
                 a += "\n";
             }
@@ -49,11 +49,11 @@ public class MainBehaviour : MonoBehaviour
     void Start()
     {
 
-        for (int i = 0; i < 22; i++)
+        for (int i = 0; i < 10; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < 22; j++)
             {
-                field[i, j] = new Block();
+                field[i, j] = 0;
                 //debugblock.GetComponent<TextMesh>().text = i + "," + j;
                 //debugblock.name = i + "," + j;
                 //Instantiate(debugblock, new Vector3(j, i, 0), prefabTetramino.transform.rotation);
@@ -71,17 +71,17 @@ public class MainBehaviour : MonoBehaviour
     {
         x = new int[4];
         y = new int[4];
-        for (int i = 0; i < 22; i++)
+        for (int i = 0; i < 10; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < 22; j++)
             {
                 for (int k = 0; k < 4; k++)
                 {
-                    if (field[i, j].Number == k + 1)
+                    if (field[i, j] == k + 1)
                     {
-                        y[k] = i;
-                        x[k] = j;
-                        if (setToZero) field[i, j].Number = 0;
+                        y[k] = j;
+                        x[k] = i;
+                        if (setToZero) field[i, j] = 0;
                     }
 
                 }
@@ -141,12 +141,12 @@ public class MainBehaviour : MonoBehaviour
 
         for (int i = 0; i < 4; i++)
         {
-            field[yy[i], xx[i]].Number = i + 1;
+            field[xx[i], yy[i]] = i + 1;
             TetrisPiece[i] = Instantiate(prefabTetramino, new Vector3(xx[i], yy[i], 0), prefabTetramino.transform.rotation);
             TetrisPiece[i].GetComponent<Renderer>().material.SetColor("_Color", color);
             TetrisPiece[i].name = "Tetramino";
             currentPiece = pieceIndex;
-            Debug.Log("spawning-" + color.ToString());
+            //Debug.Log("spawning-" + color.ToString() + xx[i] + yy[i]);
         }
 
     }
@@ -159,26 +159,26 @@ public class MainBehaviour : MonoBehaviour
             (x[1] + side >= 0 && x[1] + side <= 9) &&
             (x[2] + side >= 0 && x[2] + side <= 9) &&
             (x[3] + side >= 0 && x[3] + side <= 9) &&
-             (field[y[0], x[0] + side].Number != 5
-             && field[y[1], x[1] + side].Number != 5
-             && field[y[2], x[2] + side].Number != 5
-             && field[y[3], x[3] + side].Number != 5
+             (field[x[0] + side, y[0]] != 5
+             && field[x[1] + side, y[1]] != 5
+             && field[x[2] + side, y[2]] != 5
+             && field[x[3] + side, y[3]] != 5
              ))
         {
             //the set to zero part of the findtetraminoes method
             for (int k = 0; k < 4; k++)
             {
-                field[y[k], x[k]].Number = 0;
+                field[x[k], y[k]] = 0;
             }
 
 
             for (int i = 0; i < 4; i++)
             {
-                field[y[i], x[i] + side].Number = i + 1;
+                field[x[i] + side, y[i]] = i + 1;
                 TetrisPiece[i].transform.position = new Vector3(x[i] + side, y[i], 0);
             }
         }
-        else Debug.Log(x[0] + side);
+        //else Debug.Log(x[0] + side);
     }
     void rotate()
     {
@@ -189,10 +189,10 @@ public class MainBehaviour : MonoBehaviour
             x[1] + xx[1] >= 0 && x[1] + xx[1] <= 9 &&
             x[2] + xx[2] >= 0 && x[2] + xx[2] <= 9 &&
             x[3] + xx[3] >= 0 && x[3] + xx[3] <= 9 &&
-            field[y[0] + yy[0], x[0] + xx[0]].Number != 5 &&
-            field[y[1] + yy[1], x[1] + xx[1]].Number != 5 &&
-            field[y[2] + yy[2], x[2] + xx[2]].Number != 5 &&
-            field[y[3] + yy[3], x[3] + xx[3]].Number != 5 &&
+            field[x[0] + xx[0], y[0] + yy[0]] != 5 &&
+            field[x[1] + xx[1], y[1] + yy[1]] != 5 &&
+            field[x[2] + xx[2], y[2] + yy[2]] != 5 &&
+            field[x[3] + xx[3], y[3] + yy[3]] != 5 &&
             y[0] + yy[0] >= 0 && y[0] + yy[0] <= 21 &&
             y[1] + yy[1] >= 0 && y[1] + yy[1] <= 21 &&
             y[2] + yy[2] >= 0 && y[2] + yy[2] <= 21 &&
@@ -203,13 +203,13 @@ public class MainBehaviour : MonoBehaviour
                 //the set to zero part of the findtetraminoes method
                 for (int k = 0; k < 4; k++)
                 {
-                    field[y[k], x[k]].Number = 0;
+                    field[x[k], y[k]] = 0;
                 }
 
-                field[y[0] + yy[0], x[0] + xx[0]].Number = 1;
-                field[y[1] + yy[1], x[1] + xx[1]].Number = 2;
-                field[y[2] + yy[2], x[2] + xx[2]].Number = 3;
-                field[y[3] + yy[3], x[3] + xx[3]].Number = 4;
+                field[x[0] + xx[0], y[0] + yy[0]] = 1;
+                field[x[1] + xx[1], y[1] + yy[1]] = 2;
+                field[x[2] + xx[2], y[2] + yy[2]] = 3;
+                field[x[3] + xx[3], y[3] + yy[3]] = 4;
 
                 TetrisPiece[0].transform.position = new Vector3(x[0] + xx[0], y[0] + yy[0], 0);
                 TetrisPiece[1].transform.position = new Vector3(x[1] + xx[1], y[1] + yy[1], 0);
@@ -275,17 +275,17 @@ public class MainBehaviour : MonoBehaviour
             && y[2] - 1 >= 0
             && y[3] - 1 >= 0)
             &&
-             (field[y[0] - 1, x[0]].Number != 5
-             && field[y[1] - 1, x[1]].Number != 5
-             && field[y[2] - 1, x[2]].Number != 5
-             && field[y[3] - 1, x[3]].Number != 5
+             (field[x[0], y[0] - 1] != 5
+             && field[x[1], y[1] - 1] != 5
+             && field[x[2], y[2] - 1] != 5
+             && field[x[3], y[3] - 1] != 5
              )
             )
         {
 
             for (int i = 0; i < 4; i++)
             {
-                field[y[i] - 1, x[i]].Number = i + 1;
+                field[x[i], y[i] - 1] = i + 1;
                 TetrisPiece[i].transform.position = new Vector3(x[i], y[i] - 1, 0);
             }
 
@@ -294,34 +294,54 @@ public class MainBehaviour : MonoBehaviour
         {
             for (int i = 0; i < 4; i++)
             {
-                field[y[i], x[i]].Number = 5;
+                field[x[i], y[i]] = 5;
             }
 
             GameObject[] gameObjects = GameObject.FindObjectsOfType<GameObject>();
 
             //line detection
+
             for (int i = 0; i < 22; i++)
             {
+                
                 bool isLine = true;
                 for (int j = 0; j < 10; j++)
                 {
-                    if (field[i, j].Number != 5)
+                    if (field[j, i] != 5)
                     {
                         isLine = false;
                     }
                 }
                 if (isLine)
                 {
+                    Debug.Log(i);
                     for (int j = 0; j < 10; j++)
                     {
+                        field[j, i] = 0;
                         foreach (GameObject item in gameObjects)
                         {
-                            if (item.transform.position.x == j && item.transform.position.y == i)
-                            {
-                                item.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
-                            }
+                            if (item.transform.position.x == j && item.transform.position.y == i) { GameObject.Destroy(item); }
                         }
                     }
+
+                    int[,] temp = new int[10, 22];
+                    //21 because line 21 doesn't need to be checked because it's impossible to make a line there
+                    for (int ii = 0; ii < 10; ii++)
+                    {
+                        for (int jj = 0; jj < 21; jj++)
+                        {
+                            if (jj >= i) temp[ii, jj] = field[ii, jj + 1];
+                            else temp[ii, jj] = field[ii, jj];
+                        }
+                    }
+                    field = temp;
+
+                    foreach (GameObject item in gameObjects)
+                    {
+                        if (item.transform.position.y > i)
+                            item.transform.position = new Vector3(item.transform.position.x, item.transform.position.y - 1, item.transform.position.z);
+                    }
+                    i--;
                 }
             }
 
